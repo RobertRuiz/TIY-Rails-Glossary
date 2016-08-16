0406f951c08f520fa63c41ff9cf59902a733aca1
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-08-16 18:45:05 -0400

    Revised the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,35 +7,35 @@ Category.create(name: "Ruby",
 Term.create(name: "Active record",
             definition: "an ORM for ruby",
             author: "David Heinemeier Hansson",
-            category: 2,
+            category_id: 2,
             web_link: "http://goo.gl/dbXsUX")
 
 Term.create(name: "Modules",
             definition: "a way of grouping together methods, classes, and constants",
             author: "Andy Hunt, Dave Thomas",
-            category: 2,
+            category_id: 2,
             web_link: "http://goo.gl/cee6oc")
 
 Term.create(name: "Ruby Tk",
             definition: "a graphical user interface (GUI) for Ruby",
             author: "David Heinemeier Hansson",
-            category: 2,
+            category_id: 2,
             web_link: "http://goo.gl/JD1zMe")
 
 Term.create(name: "Strings",
             definition: "Strings are text. They are denoted by surrounding text with either single or double quotes",
             author: "MDN Mozilla Developer Network",
-            category: 1,
+            category_id: 1,
             web_link: "https://goo.gl/auKju")
 
 Term.create(name: "Classes",
             definition: "Classes are a template to create many objects with similar qualities",
             author: "MDN Mozilla Developer Network",
-            category: 1,
+            category_id: 1,
             web_link: "https://goo.gl/4yX6jM")
 
 Term.create(name: "Ternary Operator",
             definition: "The ternary operator is usually used as a shortcut for the if statement",
             author: "MDN Mozilla Developer Network",
-            category: 1,
+            category_id: 1,
             web_link: "https://goo.gl/zdZXj")
